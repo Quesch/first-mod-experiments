9930386ef29e46651f35a3e3ca6a3797f4740cc2
Quesch <115220233+Quesch@users.noreply.github.com>
2025-05-22 12:10:33 +0200

    ordered and wrote new idea

--- build/docs/javadoc/type-search-index.js
+++
@@ -1,1 +0,0 @@
-typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html"},{"p":"org.nootnoot.fabricchaos","l":"Fabricchaos"},{"p":"org.nootnoot.fabricchaos","l":"Moditemgroup"},{"p":"org.nootnoot.fabricchaos","l":"Moditems"}];updateSearchResults();
